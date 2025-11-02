e4f5ebaa31b48d72bc5d1e1abc56be82927c7d31
insane013 <69902120+insane013@users.noreply.github.com>
2025-11-03 02:08:16 +0500

    dynamic data render

---
+++ script.js
@@ -0,0 +1,51 @@
+const data = [
+      {
+        name: "Warrior",
+        items: [
+          { title: "Holly Knight", img: "https://i.pinimg.com/474x/1f/d1/ac/1fd1ac50db9b7ac7676c4125bcd53ecc.jpg", link: "#" },
+          { title: "Berserker", img: "https://i.pinimg.com/474x/1f/d1/ac/1fd1ac50db9b7ac7676c4125bcd53ecc.jpg", link: "#" },
+          { title: "Gunlancer", img: "https://i.pinimg.com/474x/1f/d1/ac/1fd1ac50db9b7ac7676c4125bcd53ecc.jpg", link: "#" }
+        ]
+      },
+      {
+        name: "Mage",
+        items: [
+          { title: "Sorcery", img: "https://i.pinimg.com/474x/1f/d1/ac/1fd1ac50db9b7ac7676c4125bcd53ecc.jpg", link: "#" },
+          { title: "Summoner", img: "https://i.pinimg.com/474x/1f/d1/ac/1fd1ac50db9b7ac7676c4125bcd53ecc.jpg", link: "#" }
+        ]
+      }
+    ];
+
+    // Функция для создания карточки
+    function createCard(item) {
+      return `
+        <div class="col-6 col-md-4 col-lg-3">
+          <a href="${item.link}" class="text-decoration-none text-light">
+            <div class="card h-100">
+              <img src="${item.img}" class="card-img-top" alt="${item.title}">
+              <div class="card-body">
+                <h5 class="card-title text-center text-light">${item.title}</h5>
+              </div>
+            </div>
+          </a>
+        </div>`;
+    }
+
+    // Функция для рендера категорий и карточек
+    function renderCategories() {
+      const container = document.getElementById('categories');
+      container.innerHTML = '';
+
+      data.forEach(category => {
+        const cardsHTML = category.items.map(createCard).join('');
+        container.innerHTML += `
+          <section class="mb-5">
+            <h2 class="mb-3">${category.name}</h2>
+            <div class="row g-3">
+              ${cardsHTML}
+            </div>
+          </section>`;
+      });
+    }
+
+    renderCategories();
